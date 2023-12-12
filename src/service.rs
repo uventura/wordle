a3668de535aeec78bedb8ad1f92176c6d8ed17d2
VOLP1 <64439930+VOLP1@users.noreply.github.com>
2023-12-12 19:25:41 -0300

    Update service.rs

--- src/service.rs
+++ src/service.rs
@@ -1,3 +1,10 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+
+
 fn validate_string(word: String, secret: String) -> [i8;5] {
     let mut validated_word: [i8;5] = [0,0,0,0,0];
 
@@ -20,4 +27,26 @@ fn validate_string(word: String, secret: String) -> [i8;5] {
         validated_word[i] = 0;
     }
     return validated_word;
+}
+pub fn palavra_aleatoria() -> String {
+    let file_path = "C:\\Users\\Eduli\\novaspalavras.txt";
+    let file = match File::open(file_path) {
+        Ok(file) => file,
+        Err(_) => return "Erro ao abrir o arquivo".to_string(),
+    };
+
+    let reader = io::BufReader::new(file);
+    let mut words = Vec::new();
+
+    for line in reader.lines() {
+        match line {
+            Ok(line) => words.extend(line.split_whitespace().map(String::from)),
+            Err(_) => continue, // Ignora linhas que não podem ser lidas
+        }
+    }
+
+    match words.choose(&mut thread_rng()) {
+        Some(word) => word.to_string(),
+        None => "Nenhuma palavra encontrada".to_string(),
+    }
 }
